6e5083b51c49e75605a43c8a1c88643a4c5695fd
Alika <dangolalika2017@gmail.com>
2024-12-15 13:52:14 +0545

    added header file

---
+++ mui-practise/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { AppBar, Button, Tab, Tabs, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+
+const Header = () => {
+  const [value, setValue] = useState(0);
+  return (
+    <>
+      <AppBar sx={{ marginBottom: "70px" }}>
+        <Toolbar>
+          <Typography sx={{ fontSize: "30px" }}>Logo</Typography>
+          <Tabs
+            sx={{ marginLeft: "auto" }}
+            textColor="inherit"
+            indicatorColor="secondary"
+            value={value}
+            onChange={(e,val)=>setValue(val)}
+          >
+            <Tab label="Home" />
+            <Tab label="About" />
+            <Tab label="Products" />
+          </Tabs>
+          <Button
+            variant="contained"
+            color="success"
+            sx={{ marginLeft: "auto" }}
+          >
+            Sign In
+          </Button>
+          
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Header;
